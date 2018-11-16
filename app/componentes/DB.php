b6c4f71657ad6c622f843219e1bab9b10a894c17
Esteves <esteves@edusites.com.br>
2018-11-15 23:58:59 -0200

    Modificado alguns metodos do tradicional mysqli_metodo, como mysqli_query(->conexao, ) por ->conexao->query() que é a forma mais legivel quando se está trabalhando com oop e tratado alguns pontos dessa classe que poderia resultar em resposta indesejadas como por exemplo não retornar dados em uma query que para estes caso agora está lançando uma excessão a ser tratada no model que está fazendo uso da classe.

--- app/componentes/DB.php
+++ app/componentes/DB.php
@@ -10,178 +10,153 @@ class DB{
 	private $senha = "12345";
 	private $conexao;
 
-	public function __construct(){
-
-		$this->conectar();	
-
+	/**
+	 * Método construtor, retorna
+	 * um objeto de conexao no momento em 
+	 * que se instância a classe.
+	 * @access public
+	 */
+		public function __construct(){
+		$this->conectar();  
 	}
-
+	/**
+	 * Método destrutor, distroi
+	 * o objeto de conexao no momento em 
+	 * que o script é finalizado, fechando
+	 * a conexao aberta.
+	 * @access public
+	 */
 	public function __destruct(){
-
-		//mysqli_close($this->conexao);
-
+		$this->conexao->close();
 	}
-
+	/**
+	 * Retorna um objeto de conexão pronto
+	 * para fazer novas consultas SQL.
+	 * @access public
+	 * @return Object do tipo mysqli Object
+	 */
 	private function conectar(){
-
 		$this->conexao = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
-
 		if ( !$this->conexao ) {
-			die('<div class="alert alert-danger" role="alert">
-				Não foi possível conectar ao mysql: ' 
-				. mysqli_connect_error() . '</div>'
-			);
-			exit();
+			throw new \Exception('Não foi possível conectar ao mysql: ' . mysqli_connect_error());
 		}else{
+				$this->conexao->set_charset("utf8");           
 			return $this->conexao;
 		}
-
 	}
-
+	
 	public function consultar($table,$campos=FALSE,$where=FALSE,$like=FALSE,$order=FALSE,$limit=FALSE){
-
-		$query = 'SELECT ';
-
-		if( empty($campos) )
-			$query.= '*';
-		else
-		 $query.= $campos;
-
-		$query.= ' FROM ' .'`'.$table.'`';
-
-		if( !empty($where) )
-		 $query.= ' WHERE ' .$where;
-
-		if( !empty($like) )
-		 $query.= ' LIKE ' .$like;
+			
+		$sql = 'SELECT ';
 		
+		if( empty($campos) )
+			$sql.= '*';
+		else
+		$sql.= $campos;
+		$sql.= ' FROM ' .'`'.$table.'`';
+		
+		if( !empty($where) )
+			$sql.= ' WHERE ' .$where;
+		if( !empty($like) )
+			$sql.= ' LIKE ' .$like;
 		if( !empty($order) )
-		 $query.= ' ORDER BY ' .$order;
-
+			$sql.= ' ORDER BY ' .$order;
 		if( !empty($limit) )
-		 $query.= ' LIMIT ' .$limit;
-
-		//die($query);
-
-		$result = mysqli_query($this->conexao,$query);
-
-		//var_dump($result->num_rows);die();
-		if ( !$result ) {
-			die('Query invalida: ' . mysqli_error($this->conexao));
-		}else if($result->num_rows >= 1){
-			//return var_dump( mysqli_fetch_assoc($result) );
-			//$row = mysqli_fetch_assoc($result);
+			$sql.= ' LIMIT ' .$limit;
+		
+		$result = $this->conexao->query($sql);
+							
+		if ( !$result->num_rows ):
+			throw new \Exception('Query não retornou nenhum registro: ' . 
+				$this->conexao->error);
+		elseif( $result->num_rows == 1 ):
+			$data = mysqli_fetch_assoc($result);
+		else:
 			while( $row =  mysqli_fetch_assoc($result) ){
 				$data[] = $row;
 			}
-			return $data;
-		}
+		endif;
 
 		// Free result set
 		mysqli_free_result($result);
-		
+				
+		return $data;
+			
+	}
+	/**
+	 * Executa uma consulta pronta
+	 * SQL. Caso seja passado um insert
+	 * retorna o último id.
+	 * @access public
+	 * @return String
+	 */
+	public function execute($sql){
+													
+		if (!$this->conexao->query($sql)):
+			throw new \Exception(' Error: na consulta SQL ' 
+			. $this->conexao->error );
+		endif;
+						
+		$last_id = $this->conexao->insert_id;
+										
+		return $last_id;
+
 
 	}
 
 	public function insert($table, $datas=array()){
-		
+			
 		foreach ($datas as $coluna => $valor) {
 			$colunas[]= '`'.$coluna.'`';
 			$valores[]= '\''.$valor.'\'';
 		}
 
+
 		$colunas = implode(', ' , $colunas);
 		$valores = implode(', ', $valores);
 
+
 		$sql ='INSERT INTO `'.$table.'` ('.$colunas.') VALUES ('.$valores.')'; 
-
-		//echo $sql;
-
-		if (mysqli_query($this->conexao, $sql)) {
-		  echo "<p class=\"bg-success\"> Valores Inserido com Sucesso! </p>";
-		  return TRUE;
-		} else {
-		  echo "<p class=\"bg-danger\"> Error: " . $sql . "<br>" . mysqli_error($this->conexao) . "</p>";
-		  return FALSE;
-		}
-
-	}
-
-	public function update($table, $datas=array(), $condition){
 		
+		if (!$this->conexao->query($sql)):
+			throw new \Exception(' Error: Não foi possível salvar os dados ' 
+			. $this->conexao->error . ' Verifique os parametros de conexão. ');
+		endif;
+		
+		$last_id = $this->conexao->insert_id;
+						
+		return $last_id;
+			
+	}
+	
+	public function update($table, $datas=array(), $condition){
+			
 		$sql = "UPDATE `$table` SET ";
-
 		foreach ($datas as $coluna => $valor) {
 			$dados[] = ' `'.$coluna.'` = \''.$valor.'\' ';
-		}	
+		}   
 		
 		$dados = implode(', ' , $dados);
-
 		$sql .= $dados; 
 		$sql .= 'WHERE '.$condition;
-
-		if (mysqli_query($this->conexao, $sql)) {
+		if ($this->conexao->query($sql)) {
 			$msg[0] = TRUE;
 			$msg[1] = "<div class=\"alert alert-success\"> <h2 class=\"text-center\">Atualização feita com sucesso!</h2> </div>";
 			return $msg;
 		} else {
 			$msg[0] = FALSE;
-			$msg[1] = "<div class=\" alert alert-danger text-center\"> <h2 class=\"text-center\">Error: " . $sql . "<br>" . mysqli_error($this->conexao) . "</h2></div>";
-		  return $msg;
+			$msg[1] = "<div class=\" alert alert-danger text-center\"> <h2 class=\"text-center\">Error: " . $sql . "
+			" . $this->conexao->error . "</h2></div>";
+			return $msg;
 		}
-
+			
 	}
-
-	public function deletar($tabela, $onde ) { //verficar com professor
-		// Montamos nossa query SQL
-		$query = "DELETE FROM `$tabela`";
-		// Caso tenhamos um valor de onde deletar dados, adicionamos a cláusula WHERE
-		if($onde) {  //(!empty($onde))
-			$query .= " WHERE $onde ";
-		}
-		die($query);
+	
+	public function deletar($tabela, $onde ) { 
+		$sql = "DELETE FROM `$tabela`  WHERE $onde ";
 		//Preparamos e executamos nossa query
-		mysqli_query($this->conexao, $query);
+		$this->conexao->query($sql);
 	}
-
-	public function numPages($tabela, $campos=FALSE, $where=FALSE) {
-
-		$query = 'SELECT ';
-
-		if( empty($campos) )
-			$query.= '*';
-		else
-		 $query.= $campos;
-
-		$query.= ' FROM ' .'`'.$tabela.'`';
-
-		if( !empty($where) )
-		 $query.= ' WHERE ' .$where;
-
-		$result = mysqli_query($this->conexao,$query);
-
-		$numObjetos = $result->num_rows;
-
-		if ( !$numObjetos ) {
-			die('Query invalida: ' . mysqli_error($this->conexao));
-		}else{	
-			//var_dump($numObjetos);die;
-			return $numObjetos;
-		}
-	}
-
+	
+	
 }
-
-
-// $_POST['cliente'] = array
-// (
-// 	'cpf' => '930202933', 
-//   'rg' => '23.098.233-12', 
-//   'dt_Nasc' => '1979-02-20', 
-//   'cnh' => '393493332', 
-//   'nome' => 'Eduardo', 
-//   'endereco' => 'Rua jdks da liem'
-// );
-
-//var_dump($_POST['cliente']);
-//$DataBase->insert('cliente', $_POST['cliente']);
