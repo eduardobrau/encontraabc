4e0fb1b2e10cce6ab0c567c9a6c0f389d948b1e9
Esteves <esteves@edusites.com.br>
2018-11-17 00:19:27 -0200

    O método construtor dessa classe não tratava nenhum erro ao ser inicializado apenas chamava uma de suas actions dinamicamente, agora porém verifica se a action informada existe dai então cria uma instancia da View e chama a action em questão inicializa caso contrário lança uma exceção e retorna ao script que fez a chamada da classe. OBS: Agora já inicializo a View sempre que a action existir, assim não preciso ficar criando esse objeto em todas as actions já que se a action existir ou não terá que retornar uma página de erro ou os dados. E a uma novidade quando se tenta buscar dados com o model, em caso de falha ou não retornar dados a um tratamento com try e catch que em caso de falha captura o erro para log em posterior implementação e retorna uma flag false para que seja tratado no controller que pode exibir uma página NOT FOUND

--- app/controllers/Usuarios.php
+++ app/controllers/Usuarios.php
@@ -5,9 +5,18 @@ use app\models\Usuario;
 use core\View;
 
 class Usuarios{
-
+  /**
+   * Ao instanciar este controller inicio
+   * a instância da classe View, visto que
+   * se o script chegou até aqui deverá mostrar algo.
+   */
   public function __construct($action,$params){
-    $this->$action($params);
+    if( method_exists($this,$action) ):
+      $this->View = new View;
+      $this->$action($params);
+    else:
+      throw new \Exception("[Error:] Pagina não encontrada");
+    endif;
   }
 
   public function index($params=null){
@@ -18,12 +27,15 @@ class Usuarios{
     $this->View->load('usuarios/index', $usuarios);
   }
 
-  public function view($params){
-    $usuario = new Usuario;
-    $this->View = new View;
-    $usuarios = $usuario->view($params['id']);
-    //echo "<pre>"; print_r($params); echo "</pre>";die;
-    $this->View->load('usuarios/view', $usuarios);
+  public function view($params){    
+    $Usuario = new Usuario;
+    $usuarios = $Usuario->view($params['id']);
+    //echo "<pre>"; print_r($usuarios); echo "</pre>";
+    if($usuarios):
+      $this->View->load('usuarios/view', $usuarios);
+    else:
+      $this->View->load('erro/error', $usuarios);
+    endif;
   }
 
 
