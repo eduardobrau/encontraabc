e6fda23ff602daf5bc68b4f28ad2502141efe472
Esteves <esteves@edusites.com.br>
2018-12-26 19:24:41 -0200

    Como anteriormente foi modificado os atributos e métodos para static dos componentes Uri e Controller, refletiu também no script inicial index.php precisando atualizar a nova forma de acesso dos métodos e propriedades do componente Controller

--- public-html/index.php
+++ public-html/index.php
@@ -14,21 +14,20 @@ use app\componentes\Helpers;
  * )
  *
 */
-$Controller = new Controller();
-//echo "<pre>"; var_dump($controller->getController()); echo "</pre>";die;
 
-if( $Controller->Uri()->isHome() ):
+//echo "<pre>"; var_dump(Controller::getController()); echo "</pre>";die;
+
+if( Controller::Uri()::isHome() ):
   //echo "<pre>"; var_dump(Uri::isHome()); echo "</pre>";
   $view = new View;
   $view->load('home');
 else:
-  $controlador = $Controller->getController();
-  $params = ( !empty($Controller->Uri()->getParams()) ) ? $Controller->Uri()->getParams() : NULL;
-  $nameSpace = "app\\controllers\\".ucfirst($controlador);
+  $params = ( !empty(Controller::Uri()::getParams()) ) ? Controller::Uri()::getParams() : NULL;
+  $nameSpace = "app\\controllers\\" . Controller::getController();
   
   if( class_exists($nameSpace) ):
     try{
-      new $nameSpace($Controller->getAction(),$params);
+      new $nameSpace(Controller::getAction(),$params);
     }catch(\Exception $e){
       $view = new View;
       $view->load('erro/error', $e->getMessage());
@@ -36,14 +35,3 @@ else:
   endif;
 
 endif;
-
-
-/**
- * O arquivo index.php na estrutura atual sempre será solicitado no browser
- * http://localhost/app_php/index.php?module=usuarios&action=index é ele
- * o script responsável por incluir o script bootstrap que primeiramente
- * carrega o arquivo de configuração config.php que é responsável pelas
- * configurações do sistema como variaveis de escopo global usado em todo o
- * sistema, e as funções de autoload encarregada de incluir as classes, componentes,
- * bibliotecas etc... Sem a necessidade de está dando require nos demais arquivos. 
-*/
