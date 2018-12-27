e7773f366aa8b348be876e40d2f026a4d5942788
Esteves <esteves@edusites.com.br>
2018-12-27 12:56:05 -0200

    Deletado a inserção de elementos dinâmicos em main.js onde estavam sendo criado e inserido 1 a 1 porque em cada evento onclick teria que ficar controlando se era para remover e adicionar esses elementos, agora crio uma constante e nela crio os elementos com uma template string onde percebi que este método é mais indicado quando se deseja criar mais de um elemento em tempo de execução por não precisar ficar controlando onde cada elemento será inserido após ou antes do elemento X com a template string crio todos os elementos normalmente podendo até identar e inserir os valores dinâmicos das variáveis e com o uso do método html() do Jquery simplesmente substituo tudo o que está no corpo da div modal-body pela a string criada dessa forma não preciso ficar removendo os elementos antes da inserção ou inserindo elemento por elemento me preocupando aonde

--- public-html/assets/js/main.js
+++ public-html/assets/js/main.js
@@ -1,44 +1,117 @@
 $(document).ready(function(){
 
-	$(".btn-danger.delete").on('click', function(){
-		var id = $(this).attr('data-id');
-		var title = $(this).attr('data-title');
+	$("button.btn-danger.delete").on('click', function(){
+		
+		const id = $(this).attr('data-id');
+		const title = $(this).attr('data-title');
 		console.log(id);
 
-		$('div.modal-body p, div.modal-footer .btn-danger').remove();
+		$('div.modal-footer .btn-danger').remove();
+		
+		$('h4#modal-title'). 
+		html('Gostaria de deletar esse dado?')
+		.attr({
+			'style':'',
+			'font-size':''
+		});
 
-		$('<p>', {
-			id: 'modalShowTitle',
-			class: 'minhaClasse',
-		})
-		.html('<span style="font-weight:700">TÍTULO</span>: ' + title)
-		.appendTo('div.modal-body');
+		const htmlElements = `
+			<p id="modalShowTitle">
+				<span style="font-weight:700">TÍTULO:</span> ${title}
+			</p>
+			<p id="modalShowId">
+				<span style="font-weight:700">ID:</span> ${id}
+			</p>
+			<div class="alert alert-danger" role="alert">
+				Uma vez deletado essa operação é inreversivel!
+			</div>
+		`;
 
-		$('<p>', {
-			id: 'modalShowId',
-			class: 'minhaClasse',
-		})
-		.html('<span style="font-weight:700">ID:</span> ' + id)
-		.appendTo('div.modal-body');
+		$('div.modal-body').html(htmlElements);
 
-		$('<p>', {
-			class: 'text-danger',
-		})
-		.html('<div class="alert alert-danger" role="alert">Uma vez deletado essa operação é inreversivel!</div>')
-		.appendTo('div.modal-body');
-
-		$('<a></a>', 
+		$('<button></button>', 
 			{
 				id: 'modalDeleteData',
 				class: 'btn btn-danger',
-				href: '#'
+				attr:		'data-id'
 			})
 		.html('Deletar')
 		.insertAfter('div.modal-footer .btn-default');
 
-		$("a#modalDeleteData").attr("href","index.php?module=usuarios&action=delete&id="+id);
-		$("#myModal").modal('show');
+		$("button#modalDeleteData")
+		.attr("data-id",id);
+		$("#modalEncontraAbc").modal('show');
+		
 	});	
+
+	$('button#loginModal').on('click', function(){
+		$('div.modal-body').html('');
+		$('div.modal-footer').html('');
+		const formLogin = `<div class="site-login">
+    
+    	<div class="account-wall">
+				<div id="logo">
+					<img class="profile-img" src="../../assets/img/logo-encontra-abc.svg" alt="" style="width:273px; height:73px;">
+				</div>
+				<form id="loginForm" action="/site/login" method="post">
+					
+					<div class="form-group row field-loginform-email required">
+						<label class="col-lg-2 control-label" for="loginform-email">Email</label>
+						<div class="col-lg-10">
+							<input type="text" id="loginform-email" class="form-control" name="LoginForm[email]" aria-required="true" aria-invalid="false">
+						</div>
+					</div>
+					
+					<div class="form-group row field-loginform-password required">
+						<label class="col-lg-2 control-label" for="loginform-password">Senha</label>
+						<div class="col-lg-10">
+							<input type="password" id="loginform-password" class="form-control" name="LoginForm[password]" aria-required="true" aria-invalid="false">
+						</div>
+					</div>
+					
+					<div id="loginError" class="alert alert-danger"><strong>Erro!</strong></div>
+					
+					<div id="btnEntrar" class="form-group row">
+						<button id="confirmEntry" type="submit" class="btn btn-primary" name="login-button">Entrar</button>
+					</div>
+
+					<div class="form-group row field-loginform-rememberme">
+						<div class="col-lg-offset-0 col-lg-9">
+							<input type="hidden" name="LoginForm[rememberMe]" value="0">
+							<input type="checkbox" id="loginform-rememberme" name="LoginForm[rememberMe]" value="1" checked=""> 
+							<label for="loginform-rememberme">Lembrar-me</label>
+						</div>
+						<div class="col-lg-12"><p class="help-block help-block-error "></p></div>
+					</div>
+
+        </form>
+        
+        <hr class="ten">
+
+        <div class="login-info">
+          <p class="text-center">Ou você pode fazer o login via Facebook.</p>
+        </div>
+        
+        <div class="login-facebook">
+					<button type="button" id="loginFacebook" class="btn btn-facebook">
+						<span class="fa fa-facebook-square" aria-hidden="true"></span>
+						Entrar com Facebook
+					</button>
+          <a class="btn btn-secondary" href="/usuarios/create">Criar uma conta</a>
+        </div>
+
+			</div>
+			</div>`; 
+			$('h4#modal-title')
+			.addClass('text-center')
+			.attr({
+				'style':'font-size:30px',
+				'text-align': 'center'
+			})
+			.html('Entrar');
+			$('div.modal-body').html(formLogin);
+			$("#modalEncontraAbc").modal('show');
+	});
 
 	$('#btnResponse').on('click', function(){
 
@@ -89,5 +162,41 @@ $(document).ready(function(){
 
 	});
 
+
+});
+
+$(document).on('click', 'button#modalDeleteData',function(){
+	alert('ok');
+	datas = {
+		url: '/usuarios/delete',
+		id: $(this).attr('data-id'),
+	}
+
+	const {url,id} = datas;
+
+	$.ajax({
+		url: url,
+		dataType: 'json',
+		type: "POST",
+		data: { id },
+		success: function(data) {
+
+			$("#modalEncontraAbc h4#modal-title")
+			.html(data.title);
+			
+			$('div.modal-body')
+			.html($('<div>',{
+				class:	''+data.class+'',
+				role:		'alert'
+			})
+			.text(data.msg));
+			
+			$("#modalEncontraAbc").modal();
+			
+			/* setTimeout( () => {
+				location.reload();
+			},3000);	 */	
+		}
+	});
 
 });
