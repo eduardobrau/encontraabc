391e46bf9852da2433f60aefcaf6f83660639bed
Esteves <esteves@edusites.com.br>
2018-12-27 19:35:09 -0200

    Feito a chamada core\Uri::retController() para obter o nome do controller atual e servir como nome de classe para as divs, section e links por exemplo, href='/<?=strtolower()?>/create' desse modo ao copiar e colar uma template index de usuarios para cidades as alterações serão bem menores.Observe que a template index.tpl.php faz uso de um array  com dois indices criado no Model um com os dados que se deseja mostrar que já é algo abtual e outro indice chamado [nav] que é um componente de navegação com todas as variáveis já prontas na hora em que é renderizada, por exemplo se o link estará disabilitado caso a página atual é igual a 1 o link para avançar e voltar na paginação

--- app/views/usuarios/index.tpl.php
+++ app/views/usuarios/index.tpl.php
@@ -1,53 +1,61 @@
-<!-- CONTAINER -->
-<div class="container">
-  <!-- ROW -->
-	<div class="row">
-    <!-- COL -->
-		<div class="col">
+<?php 
+$controller = core\Uri::retController(); 
+//echo "<pre>"; print_r($data); echo "</pre>";
+?>
 
-      <!-- <div class="alert alert-primary" role="alert"> -->
-			<div class="alert" role="alert">
-				<h1 class="text-center">Lista de Usuários</h1>
-			</div>
+<section class="wrapper-content <?=strtolower($controller)?>">
+	<div class="main">	
+		<!-- CONTAINER -->
+		<div class="container">
+			<!-- ROW -->
+			<div class="row">
+				<!-- COL -->
+				<div class="col">
 
-		  <a class="btn btn-success" href="/app_php/usuarios/create.php">Novo Usuário</a>
-			<!-- .TABLE-RESPONSIVE -->
-			<div class="table-responsive">
-				<?php
-				$table = "<table class=\"data-list table table-sm table-striped\">";
-				$table .= "<thead>";
-				$table .= "<tr>";
-				$table .= "<th>#</th><th>ID</th><th>Usuário</th><th>Senha</th><th>Email</th><th>Ações</th>";
-				$table .= "</tr>";
-				$table .= "</thead>";
-				if( $data ):
-					$i=1;
-					foreach( $data as $usuario) {
-						$table .= "<tr>"; 
-						$table .= "<td>" .$i. "</td>";
-						$table .= "<td>" .$usuario['id']. "</td>";
-						$table .= "<td>" .$usuario['usuario']. "</td>";
-						//$table .= "<td>" .$data[$i]['description']. "</td>";
-						$table .= "<td>" .$usuario['senha']. "</td>";
-						$table .= "<td>" .$usuario['email']. "</td>";
-						$table .= "<td>";
-						$table .= "<a class=\"btn btn-xs btn-success\" href='/usuarios/view/?id=".$usuario['id']."'><span class='fa fa-eye'></span></a>";
-						$table .= "<a class=\"btn btn-xs btn-warning\" href='/usuarios/edit/edit.exe?id=".$usuario['id']."'><span class='fa fa-pencil'></span></a>";
-						$table .= "<a class=\"btn btn-xs btn-danger delete\" href='#' data-id=\"".$usuario['id']."\" data-title=\"".$usuario['usuario']."\"><span class='fa fa-trash'></span></a>";
-						$table .="</td>";
+					<div class="alert title alert-primary" role="alert">
+						<h1 class="text-center">Lista de <?=$controller;?></h1>
+					</div>
+
+					<a class="btn btn-success" href="/<?=strtolower($controller)?>/create">Novo Usuário</a>
+					<!-- .TABLE-RESPONSIVE -->
+					<div class="table-responsive">
+						<?php
+						$table = "<table class=\"data-list table table-sm table-striped\">";
+						$table .= "<thead>";
+						$table .= "<tr>";
+						$table .= "<th>#</th><th>ID</th><th>Usuário</th><th>Email</th><th>Ativo</th><th>Ações</th>";
 						$table .= "</tr>";
-						$i++;
-					}
-				endif;
-				$table .= "</table>";
-				echo $table;
-				?>
-		  </div>
-			<!--./ TABLE-RESPONSIVE -->
-			
+						$table .= "</thead>";
+						if( $data['usuarios'] ):
+							$i=1;
+							foreach( $data['usuarios'] as $usuario) {
+								$table .= "<tr>"; 
+								$table .= "<td>" .$i. "</td>";
+								$table .= "<td>" .$usuario['id']. "</td>";
+								$table .= "<td>" .$usuario['usuario']. "</td>";
+								$table .= "<td>" .$usuario['email']. "</td>";
+								$table .= "<td>" .$usuario['ativo']. "</td>";
+								$table .= "<td>";
+								$table .= "<a class=\"btn btn-xs btn-success\" href='/" .strtolower($controller). "/view/?id=".$usuario['id']."'><span class='fa fa-eye'></span></a>";
+								$table .= "<a class=\"btn btn-xs btn-warning\" href='/" .strtolower($controller). "/edit/?id=".$usuario['id']."'><span class='fa fa-pencil'></span></a>";
+								$table .= "<button class=\"btn btn-xs btn-danger delete\" href='#' data-id=\"".$usuario['id']."\" data-title=\"".$usuario['usuario']."\"><span class='fa fa-trash'></span></button>";
+								$table .="</td>";
+								$table .= "</tr>";
+								$i++;
+							}
+						endif;
+						$table .= "</table>";
+						echo $table;
+						echo $data['nav'];
+						?>
+					</div>
+					<!--./ TABLE-RESPONSIVE -->
+					
+				</div>
+				<!--./ COL -->
+			</div>
+			<!--./ ROW -->
 		</div>
-    <!--./ COL -->
+		<!--./ CONTAINER -->
 	</div>
-  <!--./ ROW -->
-</div>
-<!--./ CONTAINER -->
+</section>
