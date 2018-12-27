84a5f426b005e276fec1093e0b62a97c4f19bdcf
Esteves <esteves@edusites.com.br>
2018-12-27 11:50:40 -0200

    Criado novos métodos no controller Usuarios, este controller servirá de modelo para todos os outros controller para o app. Os métodos create, edit e delete todos eles trabalham verificando o tipo de método HTTP se é POST e se atualmente uma variável ['USUARIOS'] está setada então o controller chama o método de inserção create() passando o  como parametro e lá no model terá toda a regra de validação dos dados e inserção dos dados no Banco de dados

--- app/controllers/Usuarios.php
+++ app/controllers/Usuarios.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace app\controllers;
+
 use app\models\Usuario;
-use core\View;
+use core\View; 
+use app\componentes\ErrorLog;
 
 class Usuarios{
   /**
@@ -20,9 +22,8 @@ class Usuarios{
   }
 
   public function index($params=null){
-    $usuario = new Usuario;
-    $this->View = new View;
-    $usuarios = $usuario->index();
+    $Usuario = new Usuario;
+    $usuarios = $Usuario->index();
     //echo "<pre>"; print_r($usuarios); echo "</pre>";die;
     $this->View->load('usuarios/index', $usuarios);
   }
@@ -36,6 +37,73 @@ class Usuarios{
     else:
       $this->View->load('erro/error', $usuarios);
     endif;
+  }
+
+  public function create($params=null){    
+    
+    if( $_SERVER['REQUEST_METHOD'] === 'POST' and isset($_POST['USUARIOS']) ):
+      
+      $Usuario = new Usuario;
+      
+      if( $usuario = $Usuario->create($_POST['USUARIOS']) ):
+        return $this->View->load('info/sucesso',$usuario);
+      else: 
+        $data = [
+          'title' => 'Não foi possível salvar os dados',
+          'msg'   => 'Por favor contactar o administrador do sistema'
+        ];
+        return $this->View->load('erro/error', $data);
+      endif;
+
+    endif;
+    
+    return $this->View->load('usuarios/create');    
+   
+  }
+
+  public function edit($params){
+
+    if($_SERVER['REQUEST_METHOD'] === 'GET' and isset($_GET['id'])): 
+
+      $Usuario = new Usuario;
+      /**
+       * showUser retorna todos os dados do usuário
+       * em caso de sucesso, e caso não exista ou
+       * houver um erro de SQL retornará FALSE.
+       */
+      if( $usuario = $Usuario->showUser($params['id']) ):        
+        return $this->View->load('usuarios/create',$usuario);
+      endif;
+      
+    endif;
+
+  }
+
+  public function delete($params=null){
+    
+    if( $_SERVER['REQUEST_METHOD'] === 'GET' and isset($_GET['id']) ):
+
+      $Usuario = new Usuario;
+
+      if( $userArray = $Usuario->delete($params['id']) ):        
+        //return $jSONusuario;
+        echo "<pre>"; print_r($jSONusuario); echo "</pre>";die;
+        
+      endif;
+    
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST' and isset($_POST['id'])):
+      
+      $Usuario = new Usuario;
+      
+      if( $userArray = $Usuario->delete($_POST['id']) ):        
+        
+        echo json_encode($userArray);
+        die();
+        
+      endif;
+
+    endif;
+
   }
 
 
