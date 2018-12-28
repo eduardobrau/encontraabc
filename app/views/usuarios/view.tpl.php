fc57e82413f77810f8d6cf0183b07ab31c153ba9
Esteves <esteves@edusites.com.br>
2018-12-27 22:00:14 -0200

    Capturado o controller da template view e a partir dele inserido as classes, link e titulos assim como foi feito na index

--- app/views/usuarios/view.tpl.php
+++ app/views/usuarios/view.tpl.php
@@ -1,33 +1,41 @@
-<div class="container">
-	<div class="row">
+<?php 
+$controller = core\Uri::retController(); 
+//echo "<pre>"; print_r($data); echo "</pre>";
+?>
+<section class="wrapper-content <?=strtolower($controller)?>">
+	<div class="main"> 
+    <div class="container">
+      <div class="row">
 
-		<div class="col">
-		
-			<div class="alert alert-primary" role="alert">
-				<h1 class="text-center">Visualizar Usuário</h1>
-			</div>
-			<?php 
-			
-			//( isset($_POST) ) ? var_dump($_POST['USUARIOS']) : '';
+        <div class="col">
+        
+          <div class="alert title alert-primary" role="alert">
+            <h1 class="text-center">Visualizar <?=ucfirst($controller)?></h1>
+          </div>
+          <?php 
+          
+          //( isset($_POST) ) ? var_dump($_POST['USUARIOS']) : '';
 
-			if(isset($data)):
-        $table = '<table class="table table-striped table-bordered">';
-        $table .= '<tbody>';
-        foreach ($data as $key => $valueArray) {
-          $table .= '<tr>';
-          $table .= '<td>' .$key. '</td>';
-          $table .= '<td>' .$valueArray. '</td>';
-          $table .= '</tr>';
-        }
-        $table .= '</tbody>';
-        $table .= '</table>';
-        echo $table;
-        echo '<a class="btn btn-md btn-success" href="../index/"> Voltar</a> ';
-      else:
-		  	echo "<p class=\"bg-danger\">Não Existe Esse Dado!</p>";
-      endif;
-      ?>
-		</div>
+          if(!empty($data)):
+            $table = '<table class="table table-striped table-bordered">';
+            $table .= '<tbody>';
+            foreach ($data[0] as $key => $valueArray) {
+              $table .= '<tr>';
+              $table .= '<td>' .$key. '</td>';
+              $table .= '<td>' .$valueArray. '</td>';
+              $table .= '</tr>';
+            }
+            $table .= '</tbody>';
+            $table .= '</table>';
+            echo $table;
+            echo '<a class="btn btn-md btn-success" href="../index/"> Voltar</a> ';
+          else:
+            echo "<p class=\"bg-danger\">Não Existe Esse Dado!</p>";
+          endif;
+          ?>
+        </div>
 
-	</div>
-</div>
+      </div>
+    </div>
+  </div>
+</section>
