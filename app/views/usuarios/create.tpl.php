f367c24f67e47e4d317d208472bf219e350f8a7f
Esteves <esteves@edusites.com.br>
2018-12-27 22:31:35 -0200

    Criado o template create.tpl que vai servir tanto a action create quanto edit do controller a action edit sempre espera que seja informado um ['id'] e consequentemente o método HTTP , satisfazendo essas condições imediatamente chama seu model Usuario->showUser(['id']) que caso não haja erro carrega o mesmo template da action create, passando como segundo parametro o retorno da função do model que quando carregar sua template ela vai verificar se se os valores de  vindo do controller estão ou não vazio caso não esteja atribui ao value dos inputs e cria um input type hidden passando como valor o ['id'] dessa forma posso ter um único template para as duas actions assim como a mesma função create do model também para as duas actions fazendo apenas uma verificação se o ['USUARIO']['id'] está vazio ou seja não foi setado via ['id'] e gero um id em caso verdadeiro com o novo componente GenerateUniqID

---
+++ app/views/usuarios/create.tpl.php
@@ -0,0 +1,66 @@
+<?php 
+$controller = core\Uri::retController(); 
+//echo "<pre>"; print_r($uri); echo "</pre>";
+?>
+<section class="wrapper-content">
+	<div class="main">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col">
+				
+					<div class="alert title alert-primary" role="alert">
+						<h1 class="text-center">
+						<?=(empty($_GET['id']))?'Cadastrar Novo Usuário' :'Editar Usuário';?>
+						</h1>
+					</div>
+					
+					<form id="ruleCreate" action="/<?=strtolower($controller)?>/create" accept-charset="utf-8" method="POST">
+
+						<div class="form-group">
+
+							<label for="usuario">Usuário</label>
+
+							<input type="hidden" name="USUARIOS[id]" 
+								class="form-control" pattern="[\d]{5,20}" 
+								value="<?=(!empty($_GET['id'])?$_GET['id']:NULL)?>">
+
+							<input type="text" name="USUARIOS[usuario]" 
+								class="form-control" id="usuario" placeholder="Nome de Usuário" 
+								maxlength="100" required="required" pattern="[\w]+"
+								value="<?=(!empty($data[0]['usuario'])?$data[0]['usuario']:NULL)?>">
+
+						</div>
+
+						<div class="form-group">
+
+							<label for="senha">Senha</label>
+
+							<input type="password" name="USUARIOS[senha]" 
+								class="form-control" id="senha" 
+								placeholder="Uma Senha Segura" minlength="8" maxlength="255" 
+								required="required" pattern="[\w\s]+">
+
+						</div>
+
+						<div class="form-group">
+
+							<label for="email">Email</label>
+
+							<input type="email" name="USUARIOS[email]" 
+								class="form-control" id="email" 
+								placeholder="Seu email" maxlength="145" 
+								required="required" 
+								value="<?=(!empty($data[0]['email'])?$data[0]['email']:NULL)?>">
+						</div>
+						
+						<a class="btn btn-success" href="/usuarios/index"> Voltar </a>
+						<button type="submit" class="btn btn-primary" user="action" value="create">Salvar</button>
+
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
